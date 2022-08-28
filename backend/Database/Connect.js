ae008779d56b715867bc84f68c0060437f7a1151
Tan <davidtanpro1998@gmail.com>
2022-08-28 15:32:04 +0700

    commit

--- backend/Database/Connect.js
+++ backend/Database/Connect.js
@@ -4,7 +4,6 @@ const ConnectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URL);
     console.log("Database Connected");
-    console.log(process.env.JWT_REFRESH_SECRET);
   } catch (err) {
     console.log(err);
   }
